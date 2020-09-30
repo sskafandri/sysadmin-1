3020c437c7cd9db9f3e2939d790bfacdb71cef53
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-09-30 14:48:16 -0300

    Update nodeinstall.sh

--- nodeinstall.sh
+++ nodeinstall.sh
@@ -2,7 +2,7 @@
 
 #Install NodeJS Application on CL Server
 
-yum groupinstall alt-nodejs6 alt-nodejs8 alt-nodejs9 --enablerepo=cloudlinux-updates-testing
-yum install lvemanager lve-utils ea-apache24-mod-alt-passenger --enablerepo=cloudlinux-updates-testing
-yum install cagefs --enablerepo=cloudlinux-updates-testing
-yum update lvemanager lve-utils --enablerepo=cloudlinux-updates-testing
+yum groupinstall alt-nodejs6 alt-nodejs8 alt-nodejs9 --enablerepo=cloudlinux-updates-testing -y
+yum install lvemanager lve-utils ea-apache24-mod-alt-passenger --enablerepo=cloudlinux-updates-testing -y
+yum install cagefs --enablerepo=cloudlinux-updates-testing -y
+yum update lvemanager lve-utils --enablerepo=cloudlinux-updates-testing -y
