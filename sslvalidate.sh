9e4a2b8292a1edfac77b106d7b83f46726b80480
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2021-01-04 13:41:54 -0300

    Create sslvalidate.sh

---
+++ sslvalidate.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+###############################################################################
+# Copyright 2020							                                                
+# Author: Fagner Mendes							                                          
+# License: GNU Public License						                                      
+# Version: 1.0								                                                  
+# Email: fagner.mendes22@gmail.com					                                  
+###############################################################################
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+echo ""
+
+echo "Please, inform the domain"
+read DOMAIN
+echo "Please, inform the port"
+read PORT
+
+
+echo Q|openssl s_client -connect $DOMAIN:$PORT -servername $DOMAIN -showcerts|openssl x509 -noout -dates
