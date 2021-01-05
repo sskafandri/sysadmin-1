0c80d10e9868d286ddfedeccebd3cfa7b8889834
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2021-01-05 17:25:59 -0300

    Update mod_evasive.sh

--- mod_evasive.sh
+++ mod_evasive.sh
@@ -3,16 +3,21 @@
 echo ""
 
 echo "Checking if the module is installed in this server"
-if result=$(httpd -M | grep evasive24_module 2>/dev/null); then
-    stdout=$result
-else
-    rc=$?
-    stderr=$result
 
-if [ $? -eq 0 ]; then
-echo "Mod Evasive is installed"
+if ! httpd -M | grep evasive24_module > /dev/null; then
+   echo -e "Mod Evasive is not installed! Install? (y/n) \c"
+   read
+   if "$REPLY" = "y"; then
+     yum install evasive24_module -y
+
 else
-  echo "Mod Evasive is not installed"
+
+cp /etc/apache2/conf.d/300-mod_evasive.conf /etc/apache2/conf.d/300-mod_evasive.conf-orig
+sed -i 's/DOSPageCount 4/DOSPageCount 10/g' /etc/apache2/conf.d/300-mod_evasive.conf
+sed -i 's/DOSSiteCount 100/DOSPageCount 150/g' /etc/apache2/conf.d/300-mod_evasive.conf
+sed -i 's/DOSPageInterval 1/DOSPageInterval 2/g' /etc/apache2/conf.d/300-mod_evasive.conf
+sed -i 's/DOSSiteInterval 2/DOSSiteInterval 3/g' /etc/apache2/conf.d/300-mod_evasive.conf
+
+   fi
 fi
-  fi
 
